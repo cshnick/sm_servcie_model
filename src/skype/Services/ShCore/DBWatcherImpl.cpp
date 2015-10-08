17140df41a7ed4e9588d1ea49bfbdfea7e0f8fa0
Ilia <luxa.ryabic@gmail.com>
2015-10-08 18:18:24 +0300

    DB watcher implementation

--- src/skype/Services/ShCore/DBWatcherImpl.cpp
+++ src/skype/Services/ShCore/DBWatcherImpl.cpp
@@ -1,23 +1,50 @@
 #include "DBWatcherImpl.h"
 
 #include "core/error_codes.h"
+#include "core/ref_obj_qi_ptr.h"
 #include <iostream>
 #include "common/filewatcher.h"
 #include "common/string.h"
 #include "common/string_helper.h"
 
+using Boss::RefObjPtr;
+using Boss::RefObjQIPtr;
+using Boss::IString;
+using Boss::String;
+using Boss::StringHelper;
+using Boss::Base;
+
 namespace skype_sc {
 
 class DBWatcherImplPrivate {
 	friend class DBWatcherImpl;
-	DBWatcherImplPrivate(DBWatcherImpl *p_q) : q(p_q) {
-
+	DBWatcherImplPrivate(DBWatcherImpl *p_q)
+		: q(p_q) {
+	}
+	void start() {
+		std::cout << "Mv.get" << m_w.get() << std::endl;
+		if (!m_w.get()) {
+			m_w.reset(new sm::filewatcher(m_filename, [](std::string file, int mode) {
+				std :: cout << file << "; mode: "<< (mode == sm::filewatcher::e_modify ? "modify" : "access") << std::endl;
+			}));
+		}
+		m_w->start();
+	}
+	void stop() {
+		m_w->stop();
+	}
+	void setWatchFile(std::string &&str) {
+		if (str == m_filename) {
+			return;
+		}
+		m_w.reset(nullptr);
+		m_filename = str;
 	}
 
 private:
 	DBWatcherImpl *q;
-
 	std::string m_filename;
+	std::unique_ptr<sm::filewatcher> m_w = nullptr;
 };
 
 DBWatcherImpl::DBWatcherImpl()
@@ -29,19 +56,22 @@ DBWatcherImpl::~DBWatcherImpl() {
 }
 
 Boss::RetCode BOSS_CALL DBWatcherImpl::SetWatchFile(Boss::IString *p_str) {
-	//std::string str = Boss::StringHelper(p_str);
+	d->setWatchFile(StringHelper(p_str).GetString<IString::AnsiString>());
 	return Boss::Status::Ok;
 }
-Boss::RetCode BOSS_CALL DBWatcherImpl::GetWatchFile(Boss::IString **) {
-	return Boss::Status::Ok;
+Boss::RetCode BOSS_CALL DBWatcherImpl::GetWatchFile(Boss::IString **pptr) {
+	RefObjPtr<IString> str = Base<String>::Create(d->m_filename);
+	return str.QueryInterface(pptr);
 }
 
 Boss::RetCode BOSS_CALL DBWatcherImpl::Start() {
 	std::cout << "DBWatcherImpl::Start()" << std::endl;
+	d->start();
 	return Boss::Status::Ok;
 }
 Boss::RetCode BOSS_CALL DBWatcherImpl::Stop() {
 	std::cout << "DBWatcherImpl::Stop()" << std::endl;
+	d->stop();
 	return Boss::Status::Ok;
 }
 
--- src/skype/applications/functionality_test/main.cpp
+++ src/skype/applications/functionality_test/main.cpp
@@ -3,6 +3,12 @@
 
 #include "plugin/loader.h"
 #include <iostream>
+#include <unistd.h>
+
+#include <common/string.h>
+#include "common/string_helper.h"
+
+using namespace Boss;
 
 int main()
 {
@@ -10,17 +16,16 @@ int main()
   {
     Boss::Loader Ldr("sc_reg.xml", "./" MAKE_MODULE_NAME("service_registry"),
                      "./" MAKE_MODULE_NAME("class_factory"));
-    auto serv = Boss::CreateObject<skype_sc::IDBWatcher>(skype_sc::service::id::DBWatcher);
-    serv->AddObserver();
-    Boss::RefObjQIPtr<skype_sc::IService> service(serv);
+    auto watcher = Boss::CreateObject<skype_sc::IDBWatcher>(skype_sc::service::id::DBWatcher);
+
+    watcher->SetWatchFile(Base<String>::Create("/home/ilia/.Skype/sc.ryabokon.ilia/main.db").Get());
+
+    Boss::RefObjQIPtr<skype_sc::IService> service(watcher);
     service->Start();
 
-//    auto Obj1 = Boss::CreateObject<MyNs::IFace1>(MyNs::Service::Id::Face1);
-//    if (Obj1->Method() != Boss::Status::Ok)
-//      std::cerr << "Failed to call IFace1::Method" << std::endl;
-//    auto Obj2 = Boss::CreateObject<MyNs::IFace2>(MyNs::Service::Id::Face2);
-//    if (Obj2->Method() != Boss::Status::Ok)
-//      std::cerr << "Failed to call IFace2::Method" << std::endl;
+    RefObjPtr<IString> str;
+
+    sleep(20);
   }
   catch (std::exception const &e)
   {
